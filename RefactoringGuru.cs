f0680859041583d27ad543bd71300d9096393f3e
Vitalii Litvinchuk <145115691+VitaliiLitvinchuk@users.noreply.github.com>
2024-05-08 14:53:17 +0300

    Add files via upload

---
+++ RefactoringGuru.cs
@@ -0,0 +1,63 @@
+﻿namespace RefactoringGuru
+{
+    internal class Program
+    {
+        public abstract class Component
+        {
+            public abstract string Operation();
+        }
+
+        class ConcreteComponent : Component
+        {
+            public override string Operation() => "ConcreteComponent";
+        }
+
+        abstract class Decorator(Component component) : Component
+        {
+            protected Component _component = component;
+
+            public void SetComponent(Component component)
+            {
+                _component = component;
+            }
+
+            public override string Operation()
+            {
+                if (_component != null)
+                    return _component.Operation();
+                return string.Empty;
+            }
+        }
+
+        class ConcreteDecoratorA(Component comp) : Decorator(comp)
+        {
+
+            public override string Operation() => $"ConcreteDecoratorA({base.Operation()})";
+        }
+
+        class ConcreteDecoratorB(Component comp) : Decorator(comp)
+        {
+            public override string Operation() => $"ConcreteDecoratorB({base.Operation()})";
+        }
+
+        public class Client
+        {
+            public void ClientCode(Component component) => Console.WriteLine("RESULT: " + component.Operation());
+        }
+
+        public static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            var simple = new ConcreteComponent();
+            Console.WriteLine("Client: I get a simple component:");
+            client.ClientCode(simple);
+            Console.WriteLine();
+
+            ConcreteDecoratorA decorator1 = new ConcreteDecoratorA(simple);
+            ConcreteDecoratorB decorator2 = new ConcreteDecoratorB(decorator1);
+            Console.WriteLine("Client: Now I've got a decorated component:");
+            client.ClientCode(decorator2);
+        }
+    }
+}
